a0309e3e579bae03227e1562c8d2ed64098db05b
Demon Singur <demonsingur@gmail.com>
2018-04-16 08:04:00 +0000

    msm8998-common: add ril wrapper to handle unsupported DC-HSPAP rat
    Change-Id: I6a63acab474fc5472647202125ded53abc779372

--- msm8998.mk
+++ msm8998.mk
@@ -376,6 +376,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     rild \
+    libril-wrapper \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full
