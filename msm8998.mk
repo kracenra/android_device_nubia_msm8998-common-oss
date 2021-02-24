ac2c76338662d6f6dff3cdcf0f1eb913bb40865d
Arian <arian.kulmer@web.de>
2021-02-24 22:32:45 +0100

    msm8998-common: Drop unused libs
    Change-Id: I302af2e0a1c4008cd614151a4d1d0ba10528c4b1

--- msm8998.mk
+++ msm8998.mk
@@ -163,22 +163,16 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8998 \
     memtrack.msm8998 \
     libdisplayconfig \
-    libtinyxml \
-    libqdMetaData \
     libqdMetaData.system
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
-    android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl \
-    android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    android.hardware.renderscript@1.0-impl
 
 # DPM
 PRODUCT_PACKAGES += \
