7adb77ab00a1e8f02dfa143198917cae508ec8bc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:36:05 +0200

    msm8998-common: Stop building android.hardware.broadcastradio HIDL impl
    * Nothing uses it and it isn't even in manifest
    
    Change-Id: I7e991b1c851b80a1590df20943cf8a964e03ab28

--- msm8998.mk
+++ msm8998.mk
@@ -180,9 +180,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
-    
+
 # Fingerprint feature
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
