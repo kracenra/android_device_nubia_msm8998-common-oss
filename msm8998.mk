75bc19d87b7ab86958854fbbdca5fa37d8b00858
Josh Wu <joshwu@google.com>
2022-08-18 12:36:22 +0530

    msm8998-common: Switch to AIDL Bluetooth audio
    Change-Id: Ide9598015b07aaaad6d7feae0de21938bf0a359d
    (cherry picked from commit 3186c53c8de6be6ec0ed4ef620a1eb71c3a4c6c2)
    (cherry picked from commit bf896334d3f457ffbc2980f5009ccfc4f39a2668)

--- msm8998.mk
+++ msm8998.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
