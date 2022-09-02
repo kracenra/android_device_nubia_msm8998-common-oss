LineageOS/android_device_xiaomi_msm8998-common

msm8998-common: Add a check before adding dependencies
Without a check, when performing a vendor blob regen it'll add another
dependency on top of any existing one

Change-Id: I9c7f17082fd850235c794d44cec3cda0c4b883ef

extract-files.sh
@@ -73,10 +73,14 @@ function blob_fixup() {
             sed -i 's|/product/framework/qcrilhook.jar|/system/system_ext/framework/qcrilhook.jar|g' "${2}"
             ;;
         system_ext/lib64/lib-imsvideocodec.so)
-            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM"
+            done
             ;;
         system_ext/lib64/libdpmframework.so)
-            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
+            for LIBDPM_SHIM in $(grep -L "libshim_dpmframework.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libshim_dpmframework.so" "$LIBDPM_SHIM"
+            done
             ;;
         vendor/etc/permissions/qti_libpermissions.xml)
             sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"

kracenra/android_device_nubia_msm8998-common-oss

extract-files.sh
@@ -74,7 +74,9 @@ function blob_fixup() {
             sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
             ;;
         system_ext/lib64/libdpmframework.so)
-            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
+            for LIBSHIM_DPMFRAMEWORK in $(grep -L "libshim_dpmframework.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libshim_dpmframework.so" "$LIBSHIM_DPMFRAMEWORK"
+            done
             ;;
         vendor/bin/mlipayd@1.1)
            "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"