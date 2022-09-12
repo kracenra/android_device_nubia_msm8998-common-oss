ca729d09d3478c6a746fc9244bf4954f4be40731
Cyborg2017 <wangyang0987654@gmail.com>
2022-09-12 21:19:08 +0800

    msm8998-common: Mokee-ify

--- msm8998.mk
+++ msm8998.mk
@@ -10,7 +10,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -248,8 +248,8 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay native
 #PRODUCT_PACKAGES += \
-#    vendor.lineage.livedisplay@2.0-service-sdm \
-#    vendor.lineage.livedisplay@2.0-service-sysfs
+#    vendor.mokee.livedisplay@2.0-service-sdm \
+#    vendor.mokee.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -354,7 +354,7 @@ PRODUCT_PACKAGES += \
 
 # Trust
 PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+    vendor.mokee.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
