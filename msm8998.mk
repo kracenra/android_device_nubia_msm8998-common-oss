74eb7e4a0ddb3f5dc0dfc33cdcbccc3060fa04b1
Cyborg2017 <wangyang0987654@gmail.com>
2022-10-16 01:14:31 +0800

    msm8998-common: Remove privapp-permissions-qti-product.xml

--- msm8998.mk
+++ msm8998.mk
@@ -20,7 +20,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 PRODUCT_COPY_FILES += \
