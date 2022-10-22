f5e74713d605ecc5f875e65dc1972eb488ddb32a
Cyborg2017 <wangyang0987654@gmail.com>
2022-10-22 23:04:23 +0800

    msm8998-common: Build light 2.0 hidl service

--- msm8998.mk
+++ msm8998.mk
@@ -283,6 +283,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.nubia
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
